b2a2950d516ce2784e13836a5d8cd084d21ebd98
j-coast <jcoastmail@gmail.com>
2021-10-31 21:49:44 +0100

    Add single navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import Search from './pages/Search/Search';
 import './App.css';
+import MyList from './pages/MyList/MyList';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi world!</h1>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/myList">My List</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/">
+              <Search />
+            </Route>
+            <Route path="/myList">
+              <MyList />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
